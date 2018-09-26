340d2ddd48d183604b2b24d93b525f48b165d9a2
Byreave Luo <654297651@qq.com>
2018-09-26 15:32:33 -0600

    Add prefab sliderwithvalues

---
+++ Assets/Scripts/TextControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextControl : MonoBehaviour {
+
+    // Use this for initialization
+    public Text minText;
+    public Text maxText;
+    public Text curText;
+
+    private Slider slider;
+    void Start () {
+        slider = this.GetComponentInParent<Slider>();
+        minText.text = slider.minValue.ToString();
+        maxText.text = slider.maxValue.ToString();
+        curText.text = slider.value.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        curText.text = slider.value.ToString();
+    }
+}
