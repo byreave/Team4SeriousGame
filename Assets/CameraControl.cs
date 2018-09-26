1e80da9a4fbf96370a969da8b03c137a58cbf800
Byreave Luo <654297651@qq.com>
2018-09-25 18:03:24 -0600

    add camera control and bouncy

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -6,13 +6,21 @@ public class CameraControl : MonoBehaviour {
 
 
     public GameObject ObjectToFollow;
+    public float speed = 3.0f;
+    //private Vector3 offset;
 	// Use this for initialization
 	void Start () {
-		
+        //offset = this.transform.position - ObjectToFollow.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float interpolation = speed * Time.deltaTime;
+        Vector3 pos = this.transform.position;
+        pos.x = Mathf.Lerp(this.transform.position.x, ObjectToFollow.transform.position.x, interpolation);
+        pos.y = Mathf.Lerp(this.transform.position.y, ObjectToFollow.transform.position.y, interpolation);
+
+        this.transform.position = pos;
         //this.transform.position = ObjectToFollow.transform.position;
-	}
+    }
 }
