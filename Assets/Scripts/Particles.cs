df66527338e1275e567bdc25fc881af8dbe7d94e
Travis Treadway <travis.treadway55@gmail.com>
2018-09-27 13:10:43 -0600

    Breakable Particle system

---
+++ Assets/Scripts/Particles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+    public ParticleSystem Explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnJointBreak2D(Joint2D joint)
+    {
+        
+        Instantiate(Explosion,(joint.transform.position), Quaternion.identity);
+
+        Debug.Log("nice");
+    }
+}
