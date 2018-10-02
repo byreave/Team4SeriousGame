fcb9193d0fee162f23aae1321303f072634b2bd9
Travis Treadway <travis.treadway55@gmail.com>
2018-10-02 13:06:56 -0600

    LLvl 7 in

--- Assets/Scripts/Change_Player_Mode.cs
+++ Assets/Scripts/Change_Player_Mode.cs
@@ -13,6 +13,7 @@ public class Change_Player_Mode : MonoBehaviour, IPointerDownHandler, IPointerUp
             player.GetComponent<PlayerControl>().changeMode();
             Instantiate(Ping,player.transform.localPosition,Quaternion.identity);
         }
+        Debug.Log("aa");
     }
 
     public void OnPointerUp(PointerEventData eventData)
